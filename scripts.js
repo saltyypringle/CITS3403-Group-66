afc3a6abc6c1b42e4075d0cfceb11375e543ae21
Dipika Choudhury <dipika.choudhury23@gmail.com>
2025-04-23 22:09:44 +0800

    created file for adding scripts

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
+
+window.onclick = function(event) {
+    const loginModal = document.getElementById('login-modal');
+    const signupModal = document.getElementById('signup-modal');
+    if (event.target === loginModal) {
+        loginModal.style.display = 'none';
+    }
+    if (event.target === signupModal) {
+        signupModal.style.display = 'none';
+    }
+};
