912797c18e9032bbb69bcba77e1b553f3047b7d6
HenryGod69 <celestial69death@gmail.com>
2025-04-24 12:32:52 +0800

    Css files redirected + App route initialized in python flask

--- SukiScan/sukiscan.py
+++ SukiScan/sukiscan.py
@@ -3,8 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/social")
+def social():
+    return render_template("social.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
