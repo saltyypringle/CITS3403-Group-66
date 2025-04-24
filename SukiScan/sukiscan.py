b52a7ac2e070605addd8d75c29a32d78d69af3ff
HenryGod69 <celestial69death@gmail.com>
2025-04-24 17:45:20 +0800

    Deleted flask pathway to deleted html files

--- SukiScan/sukiscan.py
+++ SukiScan/sukiscan.py
@@ -6,14 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
 @app.route("/home")
 def home():
     return render_template("home.html")
