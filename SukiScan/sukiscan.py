2ceb6eda5716beff85f7899bc30260207b550ead
HenryGod69 <celestial69death@gmail.com>
2025-04-28 19:56:47 +0800

    Made the login possible

--- SukiScan/sukiscan.py
+++ SukiScan/sukiscan.py
@@ -4,6 +4,7 @@ import os
 
 app = Flask(__name__)
 
+#Function that opens the database
 def connect_db():
     path_way_to_db = os.path.join(os.path.dirname(__file__), 'data', 'sukiscan.db')
     return sqlite3.connect(path_way_to_db)
@@ -38,6 +39,7 @@ def placeholder():
 
 @app.route("/add-details", methods=['POST'])
 def add_details():
+    #Connect to the Database
     conn = connect_db()
     cursor = conn.cursor()
     
@@ -45,12 +47,41 @@ def add_details():
     username = request.form['username']
     password = request.form['password']
     
-    query = f"INSERT INTO User (email, username, password) VALUES (?, ?, ?);"
+    query = "INSERT INTO User (email, username, password) VALUES (?, ?, ?);"
     cursor.execute(query, (email, username, password))
     
     conn.commit()
     conn.close()
     
+    return redirect(url_for('mypage'))
+
+@app.route("/logging-in", methods=['POST'])
+def logging_in():
+    conn = connect_db()
+    cursor = conn.cursor()
+    
+    username_email = request.form['email-username']
+    password = request.form['password']
+    
+    print("Received:", username_email, password)
+    
+    query = "SELECT * FROM User WHERE username = ? OR email = ?"
+    cursor.execute(query, (username_email, username_email))
+    details = cursor.fetchone()
+    
+    print(details)
+    
+    if details is None:
+        conn.close()
+        return redirect(request.referrer)
+    
+    stored_password = details[3]   
+    
+    if stored_password != password:
+        conn.close()
+        return redirect(request.referrer)
+    
+    conn.close()
     return redirect(url_for('mypage'))
 
 if __name__ == "__main__":
