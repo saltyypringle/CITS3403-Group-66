07e6d9c1f0161df34c4d6884e74092524bdd467b
HenryGod69 <celestial69death@gmail.com>
2025-05-03 19:33:17 +0800

    Still fixing problem

--- SukiScan/__init__.py
+++ SukiScan/__init__.py
@@ -8,10 +8,9 @@ login_m = LoginManager()
 login_m.init_app(app)
 login_m.login_view = "login"
 
-from models import AccountInfo
-
 @login_m.user_loader
 def get_user(id):
+    from .models import AccountInfo
     return AccountInfo.Get_Info(id)
 
 from SukiScan import routes, forms
