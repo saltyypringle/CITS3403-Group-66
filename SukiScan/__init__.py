4749ab909cf42d808ac575dabc13cc7723738d22
HenryGod69 <celestial69death@gmail.com>
2025-05-03 17:52:44 +0800

    init.py fixed

--- SukiScan/__init__.py
+++ SukiScan/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = "SukiScan"
+
+login_m = LoginManager()
+login_m.init_app(app)
+login_m.login_view = "login"
 
 from SukiScan import route
 from SukiScan import forms
