c017a509785679a7482c05f607fa08ca48617e64
HenryGod69 <celestial69death@gmail.com>
2025-05-04 18:53:09 +0800

    OMG IM SO SILLY, I FOROGT TO CHANGE TO CURRENT_USER

--- SukiScan/models.py
+++ SukiScan/models.py
@@ -1,12 +1,13 @@
 from SukiScan import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     
-    def get_id(self, UserMixin):
+    def get_id(self):
         return str(self.user_id)
     
     def __repr__(self):
