d8a65d876da80750d4532c27ba101ba63047f908
HenryGod69 <celestial69death@gmail.com>
2025-05-15 19:34:06 +0800

    DB is causing lots of conflicts

--- init_db.py
+++ init_db.py
@@ -1,5 +1,5 @@
 from SukiScan import db, app
 
 with app.app_context():
-    #  db.create_all()  # <-- Comment this out when using Flask-Migrate!
+    # db.create_all()  # <-- Comment this out when using Flask-Migrate!
     print("Database created (use Flask-Migrate for schema changes).")
